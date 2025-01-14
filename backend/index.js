ccbe2eeb883331e915a6fa2180635fdee62367a8
EdgardOrtegaPino <edgard.ortega.p@gmail.com>
2025-01-14 17:18:08 -0300

    modificacion de index.js

--- backend/index.js
+++ backend/index.js
@@ -32,34 +32,6 @@ const SECRET_KEY = process.env.SECRET_KEY;
 app.use(express.json());
 app.use(cors());
 
-// Verificar credenciales 
-
-/* const verifyCredentials = (req, res, next) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-
-    return res.status(400).send({ message: 'Email y contraseña son obligatorios.' });
-  }
-  next();
-};
-
-
-const verifyToken = (req, res, next) => {
-  const authHeader = req.headers.authorization; 
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-
-    return res.status(401).send({ message: 'Token no proporcionado o inválido.' });
-  }
-  const token = authHeader.split(' ')[1]; 
-  try {
-    const decoded = jwt.verify(token, SECRET_KEY); 
-    req.email = decoded.email;
-    next();
-  } catch (error) {
-    res.status(401).send({ message: 'Token inválido o expirado.' }); 
-  }
-};  
- */
 
 // Ruta para registrar un nuevo usuario
 app.post("/usuarios", verifyCredentials, async (req, res) => {
